49fa205c42683f58cc48c7578abfe993239fbcbb
YangHan <yhan234@qq.com>
2024-09-20 17:46:59 +0800

    workflow

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# 文心悠洛视频分析插件
+# 文心悠洛视频分析智能体
 
 使用 [flask](https://github.com/pallets/flask) 构建服务器，为[文心智能体](https://agents.baidu.com)提供接口，结合 [PaddleDetection](https://github.com/PaddlePaddle/PaddleDetection) 开发套件进行目标检测，让文心智能体拥有分析视频的能力。
 
-插件工作流程：用户向文心智能体发送 [BiliBili](https://www.bilibili.com/) 链接并要求分析视频，文心智能体调用插件接口，该插件会使用 [bilix](https://github.com/HFrost0/bilix) 下载视频，使用 [opencv-python](https://github.com/opencv/opencv-python) 从视频中截取帧，使用 [PP-YOLOE 模型](https://github.com/PaddlePaddle/PaddleDetection/blob/release/2.7/configs/ppyoloe/README_cn.md)进行目标检测，将检测结果返回给文心智能体，文心智能体总结后回复用户。
+工作流程：用户向文心智能体发送 [BiliBili](https://www.bilibili.com/) 链接并要求分析视频，文心智能体调用设置好的工作流，该工作流会使用 [bilix](https://github.com/HFrost0/bilix) 下载视频，使用 [opencv-python](https://github.com/opencv/opencv-python) 从视频中截取帧，使用 [PP-YOLOE 模型](https://github.com/PaddlePaddle/PaddleDetection/blob/release/2.7/configs/ppyoloe/README_cn.md)进行目标检测，将检测结果返回给文心智能体，文心智能体总结后回复用户。
--- server.py
+++ server.py
@@ -1,61 +1,21 @@
 #!/usr/env python3
 # -*- coding: UTF-8 -*-
 
-from flask import Flask, request, send_file, make_response
+from flask import Flask, request, make_response
 import json
 from work import download_and_detect
 
 app = Flask(__name__)
 
 
-def make_json_response(data, status_code=200):
-    response = make_response(json.dumps(data), status_code)
-    response.headers["Content-Type"] = "application/json"
-    return response
-
-
 @app.route("/detect", methods=["POST"])
-async def detect_api():
+async def detect():
     try:
         frames = await download_and_detect(request.get_json()["url"])
     except Exception as e:
-        return make_json_response({"err": "处理失败：" + str(e)})
+        return {"err": "处理失败：" + str(e)}, 200, {"Content-Type": "application/json"}
 
-    res = {
-        "frames": frames,
-        "prompt": "请不要复述目标检测结果，而是根据结果和用户说明推测一个场景，可以适当修饰，不要过度猜想。",
-    }
-
-    return make_json_response(res)
-
-
-@app.route("/ai-plugin.json")
-async def plugin_manifest():
-    host = request.host_url
-    with open("plugin_config/ai-plugin.json", encoding="utf-8") as f:
-        text = f.read().replace("PLUGIN_HOST", host)
-        return text, 200, {"Content-Type": "application/json"}
-
-
-@app.route("/openapi.yaml")
-async def openapi_spec():
-    host = request.host_url
-    with open("plugin_config/openapi.yaml", encoding="utf-8") as f:
-        text = f.read().replace("PLUGIN_HOST", host)
-        return text, 200, {"Content-Type": "text/yaml"}
-
-
-@app.route("/example.yaml")
-async def example_spec():
-    host = request.host_url
-    with open("plugin_config/example.yaml", encoding="utf-8") as f:
-        text = f.read().replace("PLUGIN_HOST", host)
-        return text, 200, {"Content-Type": "text/yaml"}
-
-
-@app.route("/logo.png")
-async def plugin_logo():
-    return send_file("plugin_config/logo.png", mimetype="image/png")
+    return {"frames": frames, "err": ""}, 200, {"Content-Type": "application/json"}
 
 
 if __name__ == "__main__":
